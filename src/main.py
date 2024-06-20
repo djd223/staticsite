f683a2d594d4d6e02788c34908b5149f140eb86c
djd223 <davissd99@LAPTOP-H9C5DSR3>
2024-06-19 18:07:16 -0700

    staticsite generator with text node and html nodes completed

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+def main():
+  node1 = TextNode("Node 1", "Italics")
+  node2 = TextNode("Node 2", "Bold", "www.google.com")
+
+  print(node1)
+  print(node2)
+
+main()
